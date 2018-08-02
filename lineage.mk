60e1abb494055c1fa1603431e01439881e840643
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-02 09:22:29 -0700

    h910: Set device and product names to elsa
    Change-Id: Ic68fb57c7436742a0b336564781bf91b1323865f

--- lineage.mk
+++ lineage.mk
@@ -9,7 +9,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_NAME := lineage_h910
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="v20" \
+    TARGET_DEVICE="elsa" \
+    PRODUCT_DEVICE="elsa" \
     PRODUCT_NAME="elsa_att_us" \
     PRIVATE_BUILD_DESC="elsa_att_us-user 7.0 NRD90M 162611117c45c release-keys"
 

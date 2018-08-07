48edbea5ee9f20769e1690ced763919e4a034c38
Eric Meddaugh <eric@meddaughs.com>
2018-08-07 18:59:26 -0400

    h910: Add TARGET_SYSTEM_PARTITION for system-as-root layout
    Change-Id: I70f2d0c6a8219aa83d379c748f8aaf8742d115e4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,7 @@ DEVICE_PATH := device/lge/h910
 -include device/lge/v20-common/BoardConfigCommon.mk
 
 TARGET_OTA_ASSERT_DEVICE := h910,us996,elsa
+TARGET_SYSTEM_PARTITION := /dev/sda18
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_h910_defconfig
